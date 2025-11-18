6d916af58681be3600146b3b8a636b7ad919a776
Yyz0305 <97648402+Yyz0305@users.noreply.github.com>
2025-11-18 18:38:56 +0800

    Add files via upload

---
+++ nav_pkg/src/wp_node.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+int main(int argc,char** argv){
+    ros::init(argc,argv,"wp_node");
+    ros::NodeHandle n;
+    
+}
